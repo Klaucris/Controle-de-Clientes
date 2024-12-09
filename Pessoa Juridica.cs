8c176989afa4210cfbb28ed8a94c7b69e29e7f41
Klaucris <159507043+Klaucris@users.noreply.github.com>
2024-12-09 02:25:25 -0300

    Update Pessoa Juridica.cs

--- Pessoa Juridica.cs
+++ Pessoa Juridica.cs
@@ -6,7 +6,7 @@ namespace Controle de Clientes
     public string ie {get; set;}
     public override void Pagar_Imposto(float v)
     {
-
+    
     this.valor = v;
     this.valor_imposto = this.valor * 20 / 100;
     this.total = this.valor + this.valor_imposto;
